bc52736f62efc3c3c36d3952f2435c8acccbce0e
Joao Luiz <contato.joaolfc@hotmail.com>
2019-05-28 15:51:39 -0300

    100%

--- assets/js/java.js
+++ assets/js/java.js
@@ -10,3 +10,14 @@ $('.slider-principal').slick({
 
 
 });	
+
+
+function toggleMenu(){
+	var menu = document.getElementById("menu");
+	if(menu.style.display == "none"){
+		menu.style.display = "block";
+	}else{
+		menu.style.display = "none";
+	}
+}
+
